7c0796dda49a5b5f9a51e35d16c81ca81e8b4838
Mo7ammedd <medo.mostafa22255@gmail.com>
2024-10-26 21:41:13 +0300

    add IBasketRepository

--- Talabat.Core/Repositories.Contract/IBasketRepository.cs
+++ Talabat.Core/Repositories.Contract/IBasketRepository.cs
@@ -1,6 +1,10 @@
-﻿namespace Talabat.Core.Repositories.Contract;
+﻿using Talabat.Core.Models;
+
+namespace Talabat.Core.Repositories.Contract;
 
 public interface IBasketRepository
 {
-    
+    Task<CustomerBasket> GetBasketAsync(string basketId);
+    Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket);
+    Task<bool> DeleteBasketAsync(string basketId);
 }
---
+++ Talabat.Repository/BasketRepository.cs
@@ -0,0 +1,22 @@
+﻿using Talabat.Core.Models;
+using Talabat.Core.Repositories.Contract;
+
+namespace Talabat.Repository;
+
+public class BasketRepository : IBasketRepository
+{
+    public BasketRepository()
+    {
+    }
+    public Task<CustomerBasket> GetBasketAsync(string basketId)
+    {
+    }
+
+    public Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket)
+    {
+    }
+
+    public Task<bool> DeleteBasketAsync(string basketId)
+    {
+    }
+}
