7bcce805d25943f49b384a7d2ae92aec97e25565
Mo7ammedd <medo.mostafa22255@gmail.com>
2024-10-26 21:36:12 +0300

    add redis-server

---
+++ Talabat.Core/Models/BasketItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Talabat.Core.Models;
+
+public class BasketItem
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string PictureUrl { get; set; }
+    public decimal Price { get; set; }
+    public string Category { get; set; }
+    public string Brand { get; set; }
+    public int Quantity { get; set; }
+}
---
+++ Talabat.Core/Models/CustomerBasket.cs
@@ -0,0 +1,8 @@
+﻿namespace Talabat.Core.Models;
+
+public class CustomerBasket
+{
+    public string id { get; set; }
+    public List<BasketItem> items { get; set; } = new List<BasketItem>();
+}
+
