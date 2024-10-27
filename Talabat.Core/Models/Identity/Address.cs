96022370f426fc6ea874183e84a4d290666503ee
Mo7ammedd <medo.mostafa22255@gmail.com>
2024-10-27 19:25:09 +0300

    add redis-server

---
+++ Talabat.Core/Models/Identity/Address.cs
@@ -0,0 +1,12 @@
+﻿namespace Talabat.Core.Models.Identity;
+
+public class Address
+{
+    public int Id { get; set; }
+    public string FName { get; set; }
+    public string LName { get; set; }
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+
+}
---
+++ Talabat.Core/Models/Identity/AppUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Talabat.Core.Models.Identity;
+
+public class AppUser : IdentityUser 
+{
+    public string DisplayName { get; set; }
+    public Address Address { get; set; }
+}
