c3675144903291f0b775964f79ff4d4048b2fa27
Mo7ammedd <medo.mostafa22255@gmail.com>
2024-10-29 19:34:42 +0300

    add OrderService

--- Talabat.Services/OrderService.cs
+++ Talabat.Services/OrderService.cs
@@ -1,6 +1,22 @@
-﻿namespace Talabat.Services;
+﻿using Talabat.Core.Models.Order_Aggregate;
+using Talabat.Core.Services.Contract;
 
-public class OrderService
+namespace Talabat.Services;
+
+public class OrderService : IOrderService
 {
-    
+    public Task<Order> CreateOrderAsync(string buyerEmail, string basketId, int deliveryMethodId, Address shippingAddress)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Order> GetOrderByIdForUserAsync(int orderId, string buyerEmail)
+    {
+        throw new NotImplementedException();
+    }
 }
