611262914d3932d414b2bd8320462a48bf72f433
Mo7ammedd <medo.mostafa22255@gmail.com>
2024-10-29 17:06:42 +0300

    add Order_Aggregate

---
+++ Talabat.Core/Models/Order Aggregate/Address.cs
@@ -0,0 +1,10 @@
+﻿namespace Talabat.Core.Models.Order_Aggregate;
+
+public class Address
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
+}
